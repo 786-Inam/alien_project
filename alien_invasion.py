e15b633b9b1f9d89d9a4f25c68ef8ca18f93eab2
Inam <awesumminam@gmail.com>
2021-01-04 12:34:55 +0530

    updated_alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ from time import sleep
 import pygame
 from settings import Settings
 from game_stats import GameStats
+from scoreboard import Scoreboard
 from button import Button
 from ship import Ship
 from bullet import Bullet
@@ -19,6 +20,7 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.stats = GameStats(self)
+        self.sb = Scoreboard(self)
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
@@ -49,8 +51,10 @@ class AlienInvasion:
     def _check_play_button(self,mouse_pos):
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
             self.aliens.empty()
             self.bullets.empty()
             self._create_fleet()
@@ -89,9 +93,15 @@ class AlienInvasion:
 
     def _check_bullet_alien_collisions(self):        
         collisions = pygame.sprite.groupcollide(self.bullets,self.aliens,True, True)
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
         if not self.aliens:
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
     
     def _create_fleet(self):
         alien = Alien(self)
@@ -157,6 +167,7 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.aliens.draw(self.screen)
+        self.sb.show_score()
         if not self.stats.game_active:
             self.play_button.draw_button()
         pygame.display.flip()
--- button.py
+++ button.py
@@ -5,9 +5,9 @@ class Button:
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.width, self.height = 200, 50
-        self.button_color = (220,250,150)
-        self.text_color = (0,0,0)
-        self.font = pygame.font.SysFont(None, 150)
+        self.button_color = (25,0,0)
+        self.text_color = (0,0,250)
+        self.font = pygame.font.SysFont(None, 48)
         self.rect = pygame.Rect(0, 0, self.width, self.height)
         self.rect.center = self.screen_rect.center
         self._prep_msg(msg)
